e107b06374c72ed25074e008352feeb73bc50125
Tsuru <200901994@qq.com>
2019-06-21 11:20:34 +0800

    1.add message listen logic;

--- chat_client/main/main.go
+++ chat_client/main/main.go
@@ -6,39 +6,46 @@ import (
 	"strings"
 )
 
-func connHandler(c net.Conn, input string) {
+func messageSend(c net.Conn, input string) {
 	if c == nil {
 		fmt.Println("Connection is closed.")
 		return
 	}
 
-	defer c.Close()
+	input = strings.TrimSpace(input)
+	c.Write([]byte(input))
+}
 
-	buf := make([]byte, 4096)
+func messageListen(c net.Conn) {
 	for {
-		input = strings.TrimSpace(input)
-		c.Write([]byte(input))
+		fmt.Println("Listening...")
+
+		buf := make([]byte, 4096)
 		cnt, err := c.Read(buf)
 		if err != nil {
 			fmt.Printf("Fail to read data, %s\n", err)
 			continue
 		}
-		fmt.Print(string(buf[0:cnt]))
+		fmt.Println(string(buf[0:cnt]))
 	}
 }
 
 func main() {
-	conn, err := net.Dial("tcp", "localhost:7777")
+	conn, err := net.Dial("tcp", "localhost:8888")
 	if err != nil {
 		fmt.Printf("Fail to connect, %s\n", err)
 		return
 	}
+	defer conn.Close()
+
+	go messageListen(conn)
+	
 	for {
 		var input string
 		fmt.Scanln(&input)
 		if input == "quit" {
-			break;
+			break
 		}
-		connHandler(conn, input)
+		messageSend(conn, input)
 	}
 }
